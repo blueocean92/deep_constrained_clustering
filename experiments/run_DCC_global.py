2e7f61c123649cb900dd342d69f3cbe5267a0215
blueocean92 <46637416+blueocean92@users.noreply.github.com>
2019-01-22 16:52:38 -0800

    Update run_DCC_global.py

--- experiments/run_DCC_global.py
+++ experiments/run_DCC_global.py
@@ -19,18 +19,16 @@ if __name__ == "__main__":
     parser.add_argument('--pretrain', type=str, default="../model/mnist_sdae_weights.pt", metavar='N',
                         help='directory for pre-trained weights')
     parser.add_argument('--data', type=str, default="MNIST", metavar='N', help='dataset(MNIST, Fashion)')
-    parser.add_argument('--use_pretrain', type=bool, default=True)
+    parser.add_argument('--use_pretrain', type=str, default="True")
     args = parser.parse_args()
-
+    
+    # Load data
     mnist_train = MNIST('./dataset/mnist', train=True, download=True)
     mnist_test = MNIST('./dataset/mnist', train=False)
     X = mnist_train.train_data
     y = mnist_train.train_labels
     test_X = mnist_test.test_data
     test_y = mnist_test.test_labels
-    ml_penalty, cl_penalty = 0.1, 1
-    idec = IDEC(input_dim=784, z_dim=10, n_clusters=10,
-                encodeLayer=[500, 500, 2000], decodeLayer=[2000, 500, 500], activation="relu", dropout=0)
     if args.data == "Fashion":
         fashionmnist_train = FashionMNIST('./dataset/fashion_mnist', train=True, download=True)
         fashionmnist_test = FashionMNIST('./dataset/fashion_mnist', train=False)
@@ -40,20 +38,32 @@ if __name__ == "__main__":
         test_y = fashionmnist_test.test_labels
         args.pretrain="../model/fashion_sdae_weights.pt"
         ml_penalty = 1
-
+        
+    # Set parameters
+    ml_penalty, cl_penalty = 0.1, 1
+    idec = IDEC(input_dim=784, z_dim=10, n_clusters=10,
+                encodeLayer=[500, 500, 2000], decodeLayer=[2000, 500, 500], activation="relu", dropout=0)
+    
+    # Print Network Structure
     print(idec)
+    if args.use_pretrain == "True":
+        idec.load_model(args.pretrain)
+        
+    # Construct constriants
     ml_ind1, ml_ind2, cl_ind1, cl_ind2 = np.array([]), np.array([]), np.array([]), np.array([])
     anchor, positive, negative = np.array([]), np.array([]), np.array([])
-    if args.use_pretrain:
-        idec.load_model(args.pretrain)
     instance_guidance = torch.zeros(X.shape[0]).cuda()
     use_global = True
+    
+    # Train the network
     train_acc, train_nmi, epo = idec.fit(anchor, positive, negative, ml_ind1, ml_ind2, cl_ind1, cl_ind2, instance_guidance, use_global, ml_penalty, cl_penalty, X, y,
                              lr=args.lr, batch_size=args.batch_size, num_epochs=args.epochs,
                              update_interval=args.update_interval,tol=1*1e-3)
+    
+    # Make predictions on test set
     test_acc, test_nmi = idec.predict(test_X, test_y)
 
-
+    # Report results
     print("Training Accuracy:", train_acc)
     print("Training NMI;", train_nmi)
     print("Training Epochs:", epo)
