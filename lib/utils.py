3416c4aec6616ad6c5e4d28afd2f911ed0f6355a
Hao-En Sung <hsung@roblox.com>
2023-03-02 21:29:28 +0000

    Get rid of dependency on `sklearn.utils.linear_assignment_`

--- lib/utils.py
+++ lib/utils.py
@@ -71,13 +71,23 @@ def acc(y_true, y_pred):
     # Return
         accuracy, in [0,1]
     """
+    def linear_assignment(cost_matrix):
+        try:
+            import lap
+            _, x, y = lap.lapjv(cost_matrix, extend_cost=True)
+            return np.array([[y[i], i] for i in x if i >= 0])
+        except ImportError:
+            from scipy.optimize import linear_sum_assignment
+            x, y = linear_sum_assignment(cost_matrix)
+            return np.array(list(zip(x, y)))
+
+
     y_true = y_true.astype(np.int64)
     assert y_pred.size == y_true.size
     D = max(y_pred.max(), y_true.max()) + 1
     w = np.zeros((D, D), dtype=np.int64)
     for i in range(y_pred.size):
         w[y_pred[i], y_true[i]] += 1
-    from sklearn.utils.linear_assignment_ import linear_assignment
     ind = linear_assignment(w.max() - w)
     return sum([w[i, j] for i, j in ind]) * 1.0 / y_pred.size
 
